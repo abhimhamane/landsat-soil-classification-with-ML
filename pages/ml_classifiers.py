11c0df6cddd1064571c6f3d01292eea62d1d890f
abhimhamane <abhi.mhamane30@gmail.com>
2022-06-29 16:59:29 +0530

    added ml classifier params form

--- pages/ml_classifiers.py
+++ pages/ml_classifiers.py
@@ -1,3 +1,7 @@
+from decimal import MIN_EMIN
+from pyexpat import model
+from click import option
+from requests import options
 import streamlit as st
 
 from pandas import read_csv
@@ -32,8 +36,63 @@ test = read_csv(r'./Clean_Data/clean_testData.csv')
 
 print(data)
 # Sidebar
-sidebar = st.sidebar
-sidebar.header("")
+st.sidebar.header("Parms")
+
+model_select = st.sidebar.selectbox(label="Select ML Classifier:", options=["KNN Classifier", "Nearest Centroid", "Decision Trees", "Random Forest", "Neural Network"])
+
+band_select = st.sidebar.form("select_band")
+band_1 = band_select.selectbox("Select Band 1:", options=['b1', 'b2', 'b3', 'b4'], index = 2)
+band_2 = band_select.selectbox("Select Band 2:", options=['b1', 'b2', 'b3', 'b4'], index = 3)
+band_select.form_submit_button("Apply Bands")
+
+model_params = st.container()
+
+if model_select == "KNN Classifier":
+    model_params.subheader(model_select)
+    knn_model_descri, knn_model_cont = model_params.columns([2, 1])
+    knn_params_form = knn_model_cont.form("KNN Params Form")
+    knn_params_form.text("KNN Params Form")
+    n_ngbrs = knn_params_form.slider("N Nearest Neighbours:", min_value=5, max_value=100, value=50, step=5)
+    weights = knn_params_form.selectbox("Weight Function:" ,options=["uniform", "distance"], index=0)
+    algo = knn_params_form.selectbox("Nearest Neighbour Algorithm:", options=['auto', 'ball_tree', 'kd_tree', 'brute'], index=0)
+    knn_params_form.form_submit_button("Apply Params")
+elif model_select == "Nearest Centroid":
+    pass
+elif model_select == "Decision Trees":
+    model_params.subheader(model_select)
+    decision_tree_descri, decision_tree_cont = model_params.columns([2, 1])
+    decision_trees_form = decision_tree_cont.form("Decision Tree Form")
+    decision_trees_form.text("Decision Tree Form")
+    critiria = decision_trees_form.selectbox("Criterion:", options=["gini", "entropy", "log_loss"], index=0)
+    max_feat = decision_trees_form.selectbox("Maximum Features:", options=["auto", "sqrt", "log2", None], index=0)
+    max_depth = decision_trees_form.selectbox("Maximum Depth:", options=[None, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    decision_trees_form.form_submit_button("Apply Params")
+
+elif model_select == "Random Forest":
+    model_params.subheader(model_select)
+    random_forest_descri, random_forest_cont = model_params.columns([2, 1])
+    random_forest_form = random_forest_cont.form("Random Forest Form")
+    random_forest_form.text("Random Forest Form")
+    n_estimate = random_forest_form.slider("n Estimators:", min_value=10, max_value=500, value=100, step=10)
+    criteria = random_forest_form.selectbox("Criterion:", options=["gini", "entropy", "log_loss"], index=0)
+    max_depth = random_forest_form.selectbox("Maximum Depth:", options = [None, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], index = 0)
+
+    random_forest_form.form_submit_button("Apply params")
+
+elif model_select == "Neural Network":
+    model_params.subheader(model_select+": Multi-layer Perceptron classifier")
+    neural_net_descri, neural_net_cont = model_params.columns([2, 1])
+    neural_net_form = neural_net_cont.form("Neural Network Form")
+    neural_net_form.text("Neural Network Form")
+    activation_func = neural_net_form.selectbox("Activation Functions:", options=['relu', 'identity', 'tanh', 'logistic'], index=0)
+    solver_algo = neural_net_form.selectbox("Solvers:", options=['adam', 'sgd', 'lbfgs'], index=0)
+    learn_rate = neural_net_form.selectbox("Learning Rate:", options=['constant', 'invscaling', 'adaptive'], index=0)
+    hidden_layer_neurons = neural_net_form.slider("Hidden Layer neurons:", min_value=1, max_value=25, value = 10)
+    depth_hidden_layer = neural_net_form.slider("Depth of Hidden Layer:", min_value = 1, max_value=10, value = 4)
+    _hidden_layers = (hidden_layer_neurons, depth_hidden_layer)
+    neural_net_form.form_submit_button("Apply params")
+
+
 
 # selecting bands for model training 
 
