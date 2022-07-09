cadc94d19507fa9ed9f1aeef2843b7c5448710cd
abhimhamane <abhi.mhamane30@gmail.com>
2022-07-09 17:48:16 +0530

    added documentations to EDA

--- pages/eda.py
+++ pages/eda.py
@@ -27,9 +27,13 @@ clean_data_train = load_cleaned_soil_data()
 data_preview_container.dataframe(train_raw.head())
 
 
+
 data = isolate_central_pxls(train_raw)
 
 data_preview_container.header("Isolating Central Pixel Values")
+data_preview_container.markdown("""
+For ease of data-handling only central pixels are used. Thus, reducing input feature size from 36 to just 4.
+""")
 data_head, data_tail = data_preview_container.columns([1,1])
 data_head.subheader("Data Head")
 data_head.dataframe(data.head())
@@ -39,31 +43,73 @@ data_tail.dataframe(data.tail())
 data_stat_cont = data_preview_container.container()
 #data_stat_choice = data_stat_cont.selectbox("Choose Stat to View:", options=['Spectral Reflectance Plot', 'Data count' ,'Data Box Plot', 'Data Scatter Plot', ])
 
-data_preview_container.subheader("Spectral Reflectance Plot")
+data_stat_cont.subheader("Spectral Reflectance Plot")
+data_stat_cont.markdown(
+"""Using this data spectral reflectance curves were plotted. Spectral Reflectance curves are
+unique for each feature, and are of vital importance in classification tasks""")
 soil_dn, soil_std = grouped_stats(data)
 spectral_plot = spectral_reflectance_plot(soil_dn, soil_std)
-data_preview_container.pyplot(spectral_plot)
+data_stat_cont.pyplot(spectral_plot)
 
-data_preview_container.subheader("Data count")
+data_stat_cont.subheader("Data count")
+count_plot, count_desc = data_stat_cont.columns([1,1])
+count_desc.markdown("""
+The dataset has 6 soil classes. 
+
+The frequency of each soil class is not same. Soil type 1 and 7
+are over represented with 1072 and 1038 examples respectively. Soil type 3, 2, 5 and 4 have
+959, 479, 470 and 415 examples respectively. 
+
+Thus, the model is going to be biased towards
+soil type 1 and 7.
+""")
+
 data_hist_plot = data_count_hist(data)
-data_preview_container.pyplot(data_hist_plot)
+count_plot.pyplot(data_hist_plot)
 
-data_preview_container.subheader("Data Box Plot - Train Data(Raw)")
+data_stat_cont.subheader("Data Box Plot - Train Data(Raw)")
+data_stat_cont.markdown(
+"""The data could be visualized through the use of Box-plots and scatter plots. 
+
+Box plots help in detecting the outliers, by presenting the 25th and 75th percentile along with the inter
+quartile range. 
+""")
 train_box_plot = data_box_plot(data, "Train Box Plot")
-data_preview_container.pyplot(train_box_plot)
+data_stat_cont.pyplot(train_box_plot)
+
+
+data_stat_cont.subheader("Data Scatter Plot")
+data_stat_cont.markdown("""
+Scatter plots help understand the presence of clustering, which often is the
+case in such classification problems.
+
+From Box-plot and pair plot it can be observed that there is natural clustering present in the
+data but there are also many outliers. Outlier removal is required.
+
+From the scatter plot it can also be observed that some band combinations are really helpful
+such as – “b1 and b3” and “b1 and b2”. The cluster identification is much easier for these
+band combinations.
+""")
+data_stat_cont.image('data_pairplot.png')
 
 
 
-data_preview_container.header("Cleaned Data")
-clean_data_head, clean_data_tail =  data_preview_container.columns([1,1])
+data_stat_cont.header("Cleaned Data")
+data_stat_cont.markdown(
+"""Data outliers can be identified if the data fall beyond the range b/w
+Q1 – 1.5(Q3-Q1) and Q3 + 1.5(Q3-Q1), where Q1 and Q3 represent the first and third
+quartiles respectively.
 
-data_preview_container.subheader("Data Box Plot - Train Data(Cleaned)")
+After removing the data outliers, cleaned data is obtained and this can be observed in the Scatter Pair Plot.
+
+
+""")
+
+clean_data_head, clean_data_tail =  data_stat_cont.columns([1,1])
+
+data_stat_cont.subheader("Data Box Plot - Train Data(Cleaned)")
 train_clean_plot = data_box_plot(clean_data_train, "Train Box Plot - Cleaned Data")
-data_preview_container.pyplot(train_clean_plot)
-
-
-data_preview_container.subheader("Data Scatter Plot")
-data_preview_container.image('data_pairplot.png')
+data_stat_cont.pyplot(train_clean_plot)
 
 
 
